7c6cdb888683b1b20ea6f26f0559a884859e7bb0
Xavi Reche <xavitecnologys@gmail.com>
2025-04-08 16:50:02 +0000

    TItulo en español y botones dinámicos

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Mi nueva app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+cantidad = st.slider("Selecciona cantidad")
+
+st.write(f'La cantidad seleccionada es: {cantidad}')
+
+for i in range(1,cantidad+1):
+    st.button(f'{i}')
